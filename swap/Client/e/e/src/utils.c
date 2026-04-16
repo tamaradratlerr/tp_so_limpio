22c9416a01dce1f0ad3b5767da00adb0a995c89c
adelgadoangio <adelgadoangio@frba.utn.edu.ar>
2026-04-16 14:07:07 +0000

    subiendo cambios swap

---
+++ swap/Client/e/e/src/utils.c
@@ -0,0 +1,125 @@
+#define _POSIX_C_SOURCE 200112L  
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h> 
+
+void* serializar_paquete(t_paquete* paquete, int bytes)
+{
+	void * magic = malloc(bytes);
+	int desplazamiento = 0;
+
+	memcpy(magic + desplazamiento, &(paquete->codigo_operacion), sizeof(int));
+	desplazamiento+= sizeof(int);
+	memcpy(magic + desplazamiento, &(paquete->buffer->size), sizeof(int));
+	desplazamiento+= sizeof(int);
+	memcpy(magic + desplazamiento, paquete->buffer->stream, paquete->buffer->size);
+	desplazamiento+= paquete->buffer->size;
+
+	return magic;
+}
+
+int crear_conexion(char *ip, char* puerto)
+{
+    struct addrinfo hints, *server_info;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    // 1. Obtener info de dirección
+    if(getaddrinfo(ip, puerto, &hints, &server_info) != 0) {
+        fprintf(stderr, "Error en getaddrinfo\n");
+        return -1;
+    }
+
+    // 2. Crear el socket
+    int socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+    if(socket_cliente == -1) {
+        freeaddrinfo(server_info);
+        return -1;
+    }
+
+    // 3. Intentar conectar
+    if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1) {
+        // Si entra acá, la conexión NO se realizó
+        perror("ERROR AL CONECTAR"); 
+        close(socket_cliente); // Cerramos el socket porque no sirve para nada
+        freeaddrinfo(server_info);
+        return -1; // Devolvemos -1 para avisar al main que falló
+    }
+
+    freeaddrinfo(server_info);
+    return socket_cliente;
+}
+
+void enviar_mensaje(char* mensaje, int socket_cliente)
+{
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+
+	paquete->codigo_operacion = MENSAJE;
+	paquete->buffer = malloc(sizeof(t_buffer));
+	paquete->buffer->size = strlen(mensaje) + 1;
+	paquete->buffer->stream = malloc(paquete->buffer->size);
+	memcpy(paquete->buffer->stream, mensaje, paquete->buffer->size);
+
+	int bytes = paquete->buffer->size + 2*sizeof(int);
+
+	void* a_enviar = serializar_paquete(paquete, bytes);
+
+	send(socket_cliente, a_enviar, bytes, 0);
+
+	free(a_enviar);
+	eliminar_paquete(paquete);
+}
+
+
+void crear_buffer(t_paquete* paquete)
+{
+	paquete->buffer = malloc(sizeof(t_buffer));
+	paquete->buffer->size = 0;
+	paquete->buffer->stream = NULL;
+}
+
+t_paquete* crear_paquete(void)
+{
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->codigo_operacion = PAQUETE;
+	crear_buffer(paquete);
+	return paquete;
+}
+
+void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio)
+{
+	paquete->buffer->stream = realloc(paquete->buffer->stream, paquete->buffer->size + tamanio + sizeof(int));
+
+	memcpy(paquete->buffer->stream + paquete->buffer->size, &tamanio, sizeof(int));
+	memcpy(paquete->buffer->stream + paquete->buffer->size + sizeof(int), valor, tamanio);
+
+	paquete->buffer->size += tamanio + sizeof(int);
+}
+
+void enviar_paquete(t_paquete* paquete, int socket_cliente)
+{
+	int bytes = paquete->buffer->size + 2*sizeof(int);
+	void* a_enviar = serializar_paquete(paquete, bytes);
+
+	send(socket_cliente, a_enviar, bytes, 0);
+
+	free(a_enviar);
+}
+
+void eliminar_paquete(t_paquete* paquete)
+{
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+}
+
+void liberar_conexion(int socket_cliente)
+{
+	close(socket_cliente);
---
+++ swap/Client/e/e/src/utils.h
@@ -0,0 +1,95 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<commons/log.h>
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	op_code codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+#ifndef UTILS_H_
+
+#define UTILS_H_
+
+
+
+#include <stdlib.h>
+
+
+
+// 1. Identificadores de las operaciones (Protocolo)
+
+typedef enum {g
+
+    INICIO_SWAP,      
+
+    LECTURA_BLOQUE,   //operacion 1
+
+    ESCRITURA_BLOQUE,  //operacion 2
+
+    RESPUESTA_OK,      //devolver confirmación (escritura)
+
+    RESPUESTA_DATOS   //devolver bytes leidos (lectura)
+
+} op_code;
+
+
+
+// 2. TAD para que SWAP informe al inicio (Tamaño total y de bloque)
+
+typedef struct {
+
+    int tamanio_total;
+
+    int tamanio_bloque;
+
+} t_handshake_swap;
+
+
+
+// 3. TAD para Lectura/Escritura (Número de bloque y datos)
+
+typedef struct {
+
+    int numero_bloque;
+
+    int bloque_size;   // Cuántos bytes mide el contenido (BLOCK_SIZE)
+
+    void* contenido;   // Los datos del proceso
+
+} t_paquete_bloque;
+
+
+
+#endif
+
+int crear_conexion(char* ip, char* puerto);
+void enviar_mensaje(char* mensaje, int socket_cliente);
+t_paquete* crear_paquete(void);
+void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio);
+void enviar_paquete(t_paquete* paquete, int socket_cliente);
+void liberar_conexion(int socket_cliente);
+void eliminar_paquete(t_paquete* paquete);
+
+#endif /* UTILS_H_ *

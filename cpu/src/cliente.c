7fd1f9c3a6d0fa3a763a0592e62608cba853a8dd
Tamara Dratler <tdratler@frba.utn.com.ar>
2026-05-19 22:43:02 -0300

    fetch, decode y algunas estructuras hechas

--- cpu/src/cliente.c
+++ cpu/src/cliente.c
@@ -24,9 +24,9 @@ int main(void)
 
     sockets.conexion_kernel_memory    = conexion_kernel_memory(config, logger, KERNEL_MEMORY);
     enviar_op_code (NUEVA_CPU, sockets.conexion_kernel_memory);
-    op_code handshake = recibir_operacion(sockets.conexion_kernel_memory); //Se espera que se devueva el op_code OK (1)
+    op_code handshake_km = recibir_operacion(sockets.conexion_kernel_memory); //Se espera que se devueva el op_code OK (1)
     
-    if(handshake = 1){
+    if(handshake_km != 1){
         log_error(logger, "Error en HandShake con Kernel Memory.");
     }
 
@@ -34,9 +34,9 @@ int main(void)
 
     sockets.conexion_memory_stick     = conexion_memory_stick(config, logger, MEMORY_STICK);
     enviar_op_code (NUEVA_CPU, sockets.conexion_memory_stick);
-    op_code handshake = recibir_operacion(sockets.conexion_memory_stick); //Se espera que se devueva el op_code OK (1)
+    op_code handshake_ms = recibir_operacion(sockets.conexion_memory_stick); //Se espera que se devueva el op_code OK (1)
     
-     if(handshake = 1){
+     if(handshake_ms != 1){
         log_error(logger, "Error en HandShake con Memory Stick.");
     }
     
@@ -44,9 +44,9 @@ int main(void)
     
     sockets.conexion_kernel_scheduler = conexion_kernelS(config, logger, KERNEL_SCHEDULER);
     enviar_op_code (NUEVA_CPU, sockets.conexion_kernel_scheduler);
-    op_code handshake = recibir_operacion(sockets.conexion_kernel_scheduler); //Se espera que se devueva el op_code OK (1)
+    op_code handshake_ks = recibir_operacion(sockets.conexion_kernel_scheduler); //Se espera que se devueva el op_code OK (1)
     
-     if(handshake = 1){
+     if(handshake_ks != 1){
         log_error(logger, "Error en HandShake con Kernel Scheduler.");
     }
 
@@ -71,14 +71,20 @@ int main(void)
     int PID = recibir_pid(sockets.conexion_kernel_scheduler);
 
     int control_loop = 1;
-    while (control_loop = 1){
-    fetch();
+    while (control_loop == 1){
+    
+        char* instruccion_raw = fetch(); 
 
-    decode();
+    if (instruccion_raw != NULL) {
+        decode(instruccion_raw); 
+    }
 
     execute();
 
     interrupciones();
+
+    liberar_instruccion(instruccion_decodificada);
+    instruccion_decodificada = NULL;
     
     }
 }
@@ -164,9 +170,92 @@ int recibir_pid (int socket_cliente){
     }
 }
 
-void fetch(); /*COMPLETAR*/
 
-void decode(); /*COMPLETAR*/
+t_contexto* contexto_actual; 
+
+void fetch() {
+    log_info(logger, "[FETCH] Solicitando instruccion para PID: %d, PC: %u", 
+             contexto_actual->pid, 
+             contexto_actual->pc);
+
+    // solicitud para la km
+    // FIJANOS QUE EL PPROTOCOLO CONCIDA CON KM (PID y PC)
+    t_paquete* paquete = crear_paquete(SOLICITUD_INSTRUCCION);
+    agregar_a_paquete(paquete, &(contexto_actual->pid), sizeof(int));
+    agregar_a_paquete(paquete, &(contexto_actual->pc), sizeof(uint32_t));
+
+    enviar_paquete(paquete, sockets.conexion_kernel_memory);
+    eliminar_paquete(paquete);
+
+    // recibimos el string de la instrucción
+    char* instruccion_raw = recibir_string(sockets.conexion_kernel_memory);
+    
+    if (instruccion_raw == NULL) {
+        log_error(logger, "Error en fetch");
+        return NULL;
+    }
+    
+    return instruccion_raw;
+
+}
+
+
+
+t_instruccion* instruccion_decodificada; 
+
+void decode(char* instruccion_raw) {
+    char** tokens = string_split(instruccion_raw, " ");
+    
+    instruccion_decodificada = malloc(sizeof(t_instruccion));
+    instruccion_decodificada->cant_params = 0;
+
+    // identificamos el tipo de instrucción (el primer token)
+    instruccion_decodificada->codigo = identificar_codigo(tokens[0]);
+
+    // cargamos los parámetros (los tokens restantes)
+    int i = 1;
+    while (tokens[i] != NULL) {
+        instruccion_decodificada->params[instruccion_decodificada->cant_params] = strdup(tokens[i]);
+        instruccion_decodificada->cant_params++;
+        i++;
+    }
+
+    // todos los free
+    string_iterate_lines(tokens, free);
+    free(tokens);
+    free(instruccion_raw);
+}
+
+// función para traducir el string al enum de instru
+
+
+/* no use switch proque cuando le pregunte a gemini si estaba bien programado me dijo
+   que: Usar if-else con strcmp es necesario en C porque los switch no pueden evaluar 
+   strings directamente*/
+
+t_instruccion_code identificar_codigo(char* token) {
+    if (strcmp(token, "NOOP") == 0)          return NOOP;
+    if (strcmp(token, "SET") == 0)           return SET;
+    if (strcmp(token, "SUM") == 0)           return SUM;
+    if (strcmp(token, "SUB") == 0)           return SUB;
+    if (strcmp(token, "JNZ") == 0)           return JNZ;
+    if (strcmp(token, "COPY_MEM") == 0)      return COPY_MEM;
+    if (strcmp(token, "MOV_IN") == 0)        return MOV_IN;
+    if (strcmp(token, "MOV_OUT") == 0)       return MOV_OUT;
+    if (strcmp(token, "MUTEX_CREATE") == 0)  return MUTEX_CREATE;
+    if (strcmp(token, "MUTEX_LOCK") == 0)    return MUTEX_LOCK;
+    if (strcmp(token, "MUTEX_UNLOCK") == 0)  return MUTEX_UNLOCK;
+    if (strcmp(token, "MEM_ALLOC") == 0)     return MEM_ALLOC;
+    if (strcmp(token, "MEM_FREE") == 0)      return MEM_FREE;
+    if (strcmp(token, "SLEEP") == 0)         return SLEEP;
+    if (strcmp(token, "STDOUT") == 0)        return STDOUT;
+    if (strcmp(token, "STDIN") == 0)         return STDIN;
+    if (strcmp(token, "INIT_PROC") == 0)     return INIT_PROC;
+    if (strcmp(token, "EXIT") == 0)          return EXIT;
+
+    // caso por defecto si no reconoce el comando
+    if (token == NULL) return EXIT;
+}
 
 void execute(); /*COMPLETAR*/
 
--- cpu/src/utils.h
+++ cpu/src/utils.h
@@ -3,4 +3,32 @@
 
 #include "../../utils/src/global_utils.h"
 
+
+
+typedef struct {
+    int pid;
+    uint32_t pc;
+    uint32_t registros[4]; // EAX, EBX, ECX, EDX, ... --- poner los que faltan
+} t_contexto;
+
+typedef enum {
+    NOOP, SET, SUM, SUB, JNZ, COPY_MEM, MOV_IN, MOV_OUT,
+    MUTEX_CREATE, MUTEX_LOCK, MUTEX_UNLOCK, 
+    MEM_ALLOC, MEM_FREE, SLEEP, 
+    STDOUT, STDIN, INIT_PROC, EXIT
+} t_instruccion_code;
+
+typedef struct {
+    t_instruccion_code codigo;
+    char* params[3]; // en los ejemplos ponían como mucho 3 parámetros
+    int cant_params;
+} t_instruccion;
+
+typedef enum{
+
+    //solicitud para km
+    SOLICITUD_INSTRUCCION
+
+}op_code;
+
 #endif /* UTILS_H_ */

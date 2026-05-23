8d00db81553a4ae40f85f782fc485dce778aaf45
christian <christianquisbert@gmail.com>
2026-05-23 00:29:59 +0000

    subo structs

--- io/src/client/utils.h
+++ io/src/client/utils.h
@@ -10,6 +10,8 @@
 #include<netdb.h>
 #include<string.h>
 #include<commons/log.h>
+#include<commons/collections/list.h>
+
 
 typedef enum
 {
@@ -22,11 +24,49 @@ typedef enum
 	SLEEP,
 } op_code;
 
+/* Este ya no va mas */
 typedef struct
 {
 	int size;
 	void* stream;
 } t_buffer;
+
+
+typedef struct {
+    uint32_t size; // Tamaño del payload
+    uint32_t offset; // Desplazamiento dentro del payload
+    void* stream; // Payload
+} t_buffer;
+
+/***** Tipos de datos que procesara el IO *****/
+/*----- STDIN -----*/
+typedef struct
+{
+	uint32_t pid;
+    uint32_t bytes_to_read;
+} t_io_stdin_recv;
+typedef struct
+{
+	uint32_t pid;
+    uint32_t input_length;
+	char* input;
+} t_io_stdin_send;
+
+/*----- STDOUT -----*/
+typedef struct
+{
+	uint32_t pid;
+    uint32_t nombre_length;
+    char* nombre;
+} t_io_stdout;
+
+/*----- SLEEP -----*/
+typedef struct
+{
+	uint32_t pid;
+    uint32_t time;
+} t_io_sleep;
+/*******************************************/
 
 typedef struct
 {
@@ -34,8 +74,6 @@ typedef struct
 	t_buffer* buffer;
 } t_paquete;
 
-
-// *** llamadas a funciones *** //
 
 //void enviar_mensaje(char* mensaje, int socket_cliente);
 
@@ -46,16 +84,18 @@ int atender_peticiones_del_KS(int, t_log*);
 
 int recibir_operacion(int);
 
-void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio);
+void agregar_a_paquete(t_paquete*, void*, int);
 
-void enviar_paquete(t_paquete* paquete, int socket_cliente);
+void* serializar_paquete(t_paquete*, int);
 
-void liberar_conexion(int socket_cliente);
+void enviar_mensaje(char*, int);
 
-void eliminar_paquete(t_paquete* paquete);
+void enviar_paquete(t_paquete*, int)
 
-void* serializar_paquete(t_paquete* paquete, int bytes);
+t_list* recibir_paquete(int);
 
-void enviar_mensaje(char* mensaje, int socket_cliente);
+void eliminar_paquete(t_paquete*);
+
+void liberar_conexion(int);
 
 #endif /* UTILS_H_ */
